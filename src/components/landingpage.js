d3c643f7344f29baf339c2d289702f84c186fb12
Patrick White <pwhite@techlinefl.com>
2018-11-01 05:26:14 -0400

    Change Image

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Grid, Cell } from 'react-mdl';
 import { PropTypes } from 'react';
 
+import PickOfMe from '../me.jpg';
+
 class Landing extends Component {
  
     render() {
@@ -9,7 +11,7 @@ class Landing extends Component {
             <div style={{width: '100%', margin: 'auto'}}>
                 <Grid className="landing-grid">
                     <Cell col={12}>
-                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRn7T9W-rOPBsjYxw5U018Vdtd1H3UrPtF5tMK6Ssr7LDSSWCGfsA" 
+                        <img src={ PickOfMe } 
                         alt="Avatar"
                         className="avatar-img" />
                         <div className="banner-text">
@@ -19,19 +21,19 @@ class Landing extends Component {
                                 HTML / CSS | Javascript | React | Angular | Python | PHP | Laravel | Node
                             </p>
                             <div className="social-links">
-                                <a href="https://google.com" rel="noopener noreferrer" target="_blank">
+                                <a href="https://www.linkedin.com/in/patrick-white-a8115419" rel="noopener noreferrer" target="_blank">
                                     <i className="fab fa-linkedin" aria-hidden="true"></i>
                                 </a>
 
-                                 <a href="https://google.com" rel="noopener noreferrer" target="_blank">
+                                 <a href="https://github.com/pwhitetechline" rel="noopener noreferrer" target="_blank">
                                     <i className="fab fa-github-square" aria-hidden="true" />
                                 </a>
 
-                                 <a href="https://google.com" rel="noopener noreferrer" target="_blank">
+                                 <a href="https://www.freecodecamp.org/fcc7c7b4032" rel="noopener noreferrer" target="_blank">
                                     <i className="fab fa-free-code-camp" aria-hidden="true" />
                                 </a>
 
-                                 <a href="https://google.com" rel="noopener noreferrer" target="_blank">
+                                 <a href="https://youtube.com" rel="noopener noreferrer" target="_blank">
                                     <i className="fab fa-youtube-square" aria-hidden="true" />
                                 </a>
                             </div>
